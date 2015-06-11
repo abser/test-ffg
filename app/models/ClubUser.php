fc3890976886926ffb5c0d974b4e0c4b382cc13c
shahul <shahul.hameed@sprim.com>
2015-06-11 13:16:07 +0530

    user update

--- app/models/ClubUser.php
+++ app/models/ClubUser.php
@@ -6,17 +6,18 @@ use LaravelBook\Ardent\Ardent;
 use Carbon\Carbon;
 
 class ClubUser extends Ardent {
-	
-    protected $fillable = array('club_id', 'user_id', 'type', 'status');
 
-	public static $rules = array(
-		'club_id'	=> 'required|integer',
-		'user_id'	=> 'required|integer',	
-		'type'		=> 'required|in:1,2,3',	
-		'status'	=> 'required|in:0,1'
-	);  
-    
+    protected $fillable = array('club_id', 'user_id', 'type', 'status');
+    protected $guarded = ['user_id'];
+    public static $rules = array(
+        'club_id' => 'required|integer',
+        'user_id' => 'required|integer',
+        'type' => 'required|in:1,2,3',
+        'status' => 'required|in:0,1'
+    );
+
     public function club() {
-    	return $this->belongsTo('Sprim\Model\Club');
+        return $this->belongsTo('Sprim\Model\Club');
     }
+
 }
