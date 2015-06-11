1d2448c6b2f3978cfe035e1091691cf99d4a5109
shahul <shahul.hameed@sprim.com>
2015-06-11 16:03:43 +0530

    user update

--- app/models/ClubUser.php
+++ app/models/ClubUser.php
@@ -7,7 +7,8 @@ use Carbon\Carbon;
 
 class ClubUser extends Ardent {
 
-    protected $guarded = array();
+   // protected $guarded = array();
+    protected $fillable = array('club_id', 'user_id', 'type', 'status','created_by');
 
     public function club() {
         return $this->belongsTo('Sprim\Model\Club');
