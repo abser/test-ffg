c5fa2adacb6e18324d7b157b9c5fc0959e812133
Vijay Hegde <vijay.hegde@sprim.com>
2015-06-11 19:48:27 +0530

    admin update

--- app/database/migrations/2015_05_28_060619_profile_contacts.php
+++ app/database/migrations/2015_05_28_060619_profile_contacts.php
@@ -12,19 +12,28 @@ class ProfileContacts extends Migration {
      */
     public function up() {
         Schema::create('profile_contacts', function(Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned()->nullable();
-            $table->enum('contact_type', array('1', '2', '3'))->default(1);
-            $table->enum('is_private', array('0', '1'))->default(0);
-            $table->string('info');
-            $table->integer('created_by')->unsigned();
-            $table->integer('updated_by')->unsigned()->nullable();
+            $table->increments		('id');
+            $table->integer			('user_id')			->unsigned()->nullable();
+            $table->tinyInteger	    ('type')			->nullable();
+            $table->integer			('profile_id')		->unsigned()->nullable();
+            $table->enum			('contact_type', array('1', '2', '3'))->default(1);
+            $table->enum			('is_private', array('0', '1'))->default(0);
+            $table->string			('info');
+            
+            $table->integer			('created_by')		->unsigned();
+            $table->integer			('updated_by')		->unsigned()->nullable();
             $table->timestamps();
             $table->softDeletes();
 
             $table->foreign('user_id')
                     ->references('id')->on('users')
                     ->onDelete('cascade');
+                      
+            $table->foreign('profile_id')
+            ->references('id')->on('profiles')
+            ->onDelete('cascade');
+            
+            
         });
     }
 
