dc037c7b7705afa7594bafc08d2812ba6801b464
Vijay Hegde <vijay.hegde@sprim.com>
2015-06-12 09:38:09 +0530

    new session param added

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,9 +3,13 @@ use Sprim\Repositories\Contracts\UserInterface as User;
 
 class HomeController extends BaseController {
 
-	public function __construct() 
+	public function __construct(User $user) 
     {
-        
+    	$this->user		= $user;
+    	 
+    	$this->owner_table = Config::get('sprim.tables.profile');
+    	$this->sort      = 'first_name';
+    	$this->dir       = 'asc';
     }  
     
 	public function index()
@@ -13,6 +17,14 @@ class HomeController extends BaseController {
         $data['role']   = 'user';
         
         if (Session::get('user.is_member')) {
+        	
+        	$limit = 6;
+        	
+        	$query = $this->user->filteredModel(null, null, array(3,4,5));
+        	 
+        	$data['totalItems'] = count($query->get());
+        	$data['items']		= $query->skip(0)->take($limit)->get();
+        	       	
 			return View::make('site.member.index', compact('data'));
         } else {
         	return View::make('admin.dashboard', compact('data'));
