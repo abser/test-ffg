c21c5cc30ec9cf2aca34318f990c6244782dd1c2
Vijay Hegde <vijay.hegde@sprim.com>
2015-06-12 12:30:08 +0530

    fixes admin wellness

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,11 +1,15 @@
 <?php
 use Sprim\Repositories\Contracts\UserInterface as User;
+use Sprim\Repositories\Contracts\FileInterface as File;
+use Sprim\Repositories\Contracts\FileOwnerInterface as FileOwner;
 
 class HomeController extends BaseController {
 
-	public function __construct(User $user) 
+	public function __construct(User $user, File $file, FileOwner $file_owner) 
     {
-    	$this->user		= $user;
+    	$this->user				= $user;
+    	$this->file				= $file;
+    	$this->file_owner		= $file_owner;
     	 
     	$this->owner_table = Config::get('sprim.tables.profile');
     	$this->sort      = 'first_name';
@@ -24,7 +28,12 @@ class HomeController extends BaseController {
         	 
         	$data['totalItems'] = count($query->get());
         	$data['items']		= $query->skip(0)->take($limit)->get();
-        	       	
+        	
+        	$data['pics'] = [];
+        	foreach ($data['items'] as $row) {        		
+        		$data['pics'][$row->id]    = $this->file_owner->getFile($row->id, $this->owner_table, $this->file_type['avatar']);
+        	}
+        	        	       	
 			return View::make('site.member.index', compact('data'));
         } else {
         	return View::make('admin.dashboard', compact('data'));
